64c52556123f1edc3b303804d1bbac1875ce05c8
Dinara Khisametdinova <khdinova@gmail.com>
2024-11-10 02:49:29 +0300

    Changed basic_math.py

--- lab0/basic_math.py
+++ lab0/basic_math.py
@@ -1,5 +1,8 @@
 import numpy as np
 import scipy as sc
+from scipy.optimize import minimize_scalar
+from scipy.stats import skew
+from scipy.stats import kurtosis
 
 
 def matrix_multiplication(matrix_a, matrix_b):
@@ -7,8 +10,21 @@ def matrix_multiplication(matrix_a, matrix_b):
     Задание 1. Функция для перемножения матриц с помощью списков и циклов.
     Вернуть нужно матрицу в формате списка.
     """
-    # put your code here
-    pass
+    if len(matrix_a[0]) != len(matrix_b):
+        raise ValueError("Число столбцов первой матрицы должно быть равно числу строк второй матрицы.")
+
+    # Размерность результата
+    result_rows = len(matrix_a)
+    result_cols = len(matrix_b[0])
+
+    result = [[0 for _ in range(result_cols)] for _ in range(result_rows)] # 0 матрица
+
+    for i in range(result_rows):
+        for k in range(result_cols):
+            for j in range(len(matrix_b)):
+                result[i][k] += matrix_a[i][j] * matrix_b[j][k]
+    
+    return result
 
 
 def functions(a_1, a_2):
@@ -17,17 +33,34 @@ def functions(a_1, a_2):
     Необходимо найти точки экстремума функции и определить, есть ли у функций общие решения.
     Вернуть нужно координаты найденных решения списком, если они есть. None, если их бесконечно много.
     """
-    # put your code here
-    pass
-
+    coeff1 = list(map(float, a_1.split()))
+    coeff2 = list(map(float, a_2.split()))
+    
+    func1 = lambda x: coeff1[0] * x**2 + coeff1[1] * x + coeff1[2]
+    func2 = lambda x: coeff2[0] * x**2 + coeff2[1] * x + coeff2[2]
+    
+    extremum1 = minimize_scalar(func1)
+    extremum2 = minimize_scalar(func2)
+    
+    if extremum1.success and extremum2.success:
+        x1, y1 = extremum1.x, func1(extremum1.x)
+        x2, y2 = extremum2.x, func2(extremum2.x)
+        
+        if np.isclose(x1, x2) and np.isclose(y1, y2):
+            return [(x1, y1)]  
+        elif np.isclose(y1, y2):
+            return None  
+        else:
+            return []  
+    return [] 
 
 def skew(x):
     """
     Задание 3. Функция для расчета коэффициента асимметрии.
     Необходимо вернуть значение коэффициента асимметрии, округленное до 2 знаков после запятой.
     """
-    # put your code here
-    pass
+    return round(skew(x), 2)
+
 
 
 def kurtosis(x):
@@ -35,5 +68,6 @@ def kurtosis(x):
     Задание 3. Функция для расчета коэффициента эксцесса.
     Необходимо вернуть значение коэффициента эксцесса, округленное до 2 знаков после запятой.
     """
-    # put your code here
-    pass
+    return round(kurtosis(x), 2)
+
+    
